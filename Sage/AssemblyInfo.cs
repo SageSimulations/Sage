d77acb70c4dc2b01690cec54f0ab7d6ed19f5d46
Pete Bosch <pbosch@highpointsoftware.com>
2019-03-16 10:10:49 -0500

    Removed SharpZipLib, and incremented to version 4.0.1

--- Sage/AssemblyInfo.cs
+++ Sage/AssemblyInfo.cs
@@ -26,4 +26,4 @@ using System.Reflection;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.0.0.*")]
+[assembly: AssemblyVersion("4.0.1.*")]
