SageSimulations/Sage

commit a0546fe38a1ee4441f9f120ad7076d5ebde8a56b

RenderAbort, EventMove, -DistroCat, +WorkflowUpdate
Added the ability to abort the rendering process.
Added the ability to move an event from one place in the event queue to another (i.e. to change it's exec time.)
Removed the Distribution Catalog.
Removed Expressions & Evaluator.
Numerous code formatting updates.
Added a sample for workflow update and re-execution.

Sage_Aux/SageTestLib/TestSmartPropertyBag.cs
@@ -144,7 +144,6 @@ namespace Highpoint.Sage.SimCore  {
 			m_steve = 12;
 			SmartPropertyBag spb1 = new SmartPropertyBag();
 			spb1.AddValue("Fred",12);
-			spb1.AddExpression("Bill","Math.Max(Fred,13)+15",new string[]{"Fred"});
 			spb1.AddDelegate("Steve",new SmartPropertyBag.SPBDoubleDelegate(ComputeSteve));
 			spb1.AddString("Name","SPB1");
             
@@ -155,7 +154,6 @@ namespace Highpoint.Sage.SimCore  {
 			Debug.WriteLine("The new snapshot is " + (mem1==mem2?"":"not ") + "equal to the one preceding it.\r\n");
             Assert.IsTrue(mem2 == mem1,"Memento 1 is not equal Memento 2");
 
-            Assert.IsTrue((double)spb1["Bill"] == 28,"Bill is not 28");
             Assert.IsTrue((double)spb1["Steve"] == 12,"Steve is not 12");
             Assert.IsTrue("SPB1".Equals(spb1["Name"]),"Name is not SPB1");
 
@@ -174,16 +172,11 @@ namespace Highpoint.Sage.SimCore  {
 			Debug.WriteLine("The new snapshot is " + (mem2==mem3a?"":"not ") + "equal to the one preceding it.\r\n");
             Assert.IsTrue(mem2!=mem3a,"Memento 2 is equal to memento 3a");
 
-            Assert.IsTrue((double)spb1["Bill"] == 29,"Bill is not 29");	// changing Fred to 14 on line 120 changes Bill too
-
 			spb1.AddValue("_Connie",99);
 
 			SmartPropertyBag spb2 = new SmartPropertyBag();
 			spb2.AddValue("_Pete",14);
 			spb1.AddAlias("Pete",spb2,"_Pete");
-			spb1.AddExpression("Marvin","Pete+Bill",new string[]{"Pete","Bill"});
-            Assert.IsTrue((double)spb1["Marvin"] == 43,"Marvin is not 43");
-
 
 			Highpoint.Sage.Utility.Mementos.IMemento mem4 = spb1.Memento;
 			Highpoint.Sage.Utility.Mementos.IMemento mem5 = spb1.Memento;
@@ -237,7 +230,7 @@ namespace Highpoint.Sage.SimCore  {
 		public void TestMementoRestorationAndEquality(){
             SmartPropertyBag spb1 = new SmartPropertyBag();
             spb1.AddValue("Fred",12);
-            spb1.AddExpression("Bill","Math.Max(Fred,17)",new string[]{"Fred"});
+            //spb1.AddExpression("Bill","Math.Max(Fred,17)",new string[]{"Fred"});
             spb1.AddDelegate("Steve",new SmartPropertyBag.SPBDoubleDelegate(ComputeSteve));
             spb1.AddString("Donkey","Kong");
             spb1.AddBoolean("HabaneroHot",true);

Sage/Core/ExecController.cs
@@ -197,8 +197,12 @@ namespace Highpoint.Sage.SimCore {
             if ( iExecutive != m_executive ) throw new InvalidOperationException("ExecController is starting within a model whose executive is not the same one to which it was initialized.");
             m_executive.ClockAboutToChange -= m_doThrottle; // In case we were listening from an earlier run.
             m_executive.ClockAboutToChange += m_doThrottle;
-            if (m_renderThread != null && m_renderThread.ThreadState == ThreadState.Running) {
-                m_renderThread.Abort(); 
+            if (m_renderThread != null && m_renderThread.ThreadState == ThreadState.Running)
+            {
+                m_abortRendering = true;
+                // ReSharper disable once EmptyEmbeddedStatement
+                while (m_renderThread.IsAlive);
+                m_abortRendering = false;
             }
             m_renderThread = new Thread(RunRendering)
             {
@@ -243,10 +247,11 @@ namespace Highpoint.Sage.SimCore {
 
 
         private bool m_renderPending;
+        private bool m_abortRendering = false;
         private void RunRendering() {
             _Debug.Assert(Thread.CurrentThread.Equals(m_renderThread));
 
-            while (true) {
+            while (!m_abortRendering) {
                 if (m_executive.State.Equals(ExecState.Running)) {
                     int nTicksToSleep = 500; // Check to see if we've changed frame rate from zero, every half-second.
                     if (m_frameRate > 0)

Sage/Core/ExecFactory.cs
@@ -146,12 +146,14 @@ namespace Highpoint.Sage.SimCore {
         public IExecutive CreateExecutive(Guid execGuid) {
             if (m_requiredType == null) {
                 m_requiredType = "Highpoint.Sage.SimCore.Executive, Sage";
+#if !NETSTANDARD
                 NameValueCollection nvc = (NameValueCollection)ConfigurationManager.GetSection("Sage");
                 if (nvc == null) {
                     _Debug.WriteLine("Warning - <Sage> section missing from config file for " + Process.GetCurrentProcess().ProcessName);
                 } else if (nvc["ExecutiveType"] != null) {
                     m_requiredType = nvc["ExecutiveType"];
                 }
+#endif
             }
             return CreateExecutive(m_requiredType, execGuid);
         }

Sage/Core/Executive.cs
@@ -49,6 +49,7 @@ namespace Highpoint.Sage.SimCore {
             m_guid = execGuid;
             m_currentEventType = ExecEventType.None;
 
+#if !NETSTANDARD
             #region >>> Set up from-config-file parameters <<<
             int desiredMinWorkerThreads = 100;
             int desiredMaxWorkerThreads = 900;
@@ -103,8 +104,8 @@ namespace Highpoint.Sage.SimCore {
                     _Debug.WriteLine(msg);
                 }
             }
-
-            #endregion
+#endregion
+#endif
         }
 
         private static void Swap(ref int a, ref int b) { int tmp = a; a = b; b = tmp; }
@@ -250,6 +251,31 @@ NOTE - the engine will still run, we'll just ignore it if an event is requested 
         /// <returns>A long, which is a number that serves as a key. This key is used, for example, to unrequest the event.</returns>
         public long RequestEvent(ExecEventReceiver eer, DateTime when, double priority, object userData, ExecEventType execEventType){
             return RequestEvent(eer,when,priority,userData,execEventType,false);
+        }
+
+        /// <summary>
+        /// Creates an event that looks like an event already in queue, but for a new time. Optionally deletes the old event.
+        /// </summary>
+        /// <param name="eventID">The ID number assigned to the event when it was initially requested.</param>
+        /// <param name="newTime">The new time.</param>
+        /// <param name="deleteOldOne">The old event will be removed.</param>
+        /// <returns>A code that can subsequently be used to identify the request, e.g. for removal.</returns>
+        public long ResubmitEventAtTime(long eventID, DateTime newTime, bool deleteOldOne)
+        {
+            long newKey = long.MinValue;
+            foreach (IExecEvent @event in EventList)
+            {
+                if (@event.Key == eventID)
+                {
+                    newKey = RequestEvent(@event.ExecEventReceiver, newTime, @event.Priority, @event.UserData,
+                        @event.EventType, @event.IsDaemon);
+                    if ( deleteOldOne ) UnRequestEvent(eventID);
+                    break;
+                }
+
+            }
+            return newKey;
+
         }
 
         /// <summary>
@@ -1151,7 +1177,7 @@ NOTE - the engine will still run, we'll just ignore it if an event is requested 
             ServiceCompleted = null;
         }
 
-        public bool IsDaemon;
+        public bool IsDaemon { get; set; }
         public ExecEventReceiver Eer;
         public DateTime m_when;
         public double m_priority;
@@ -1236,6 +1262,13 @@ NOTE - the engine will still run, we'll just ignore it if an event is requested 
         /// </summary>
         /// <value>The key.</value>
         long Key { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether this instance is a daemon event.
+        /// </summary>
+        /// <value><c>true</c> if this instance is daemon; otherwise, <c>false</c>.</value>
+        bool IsDaemon { get; }
+    
         /// <summary>
         /// Returns a <see cref="System.String" /> that represents this instance.
         /// </summary>

Sage/Core/ExecutiveFastLight.cs
@@ -142,6 +142,7 @@ namespace Highpoint.Sage.SimCore {
         /// </summary>
         /// <param name="execGuid">The GUID by which this executive will be known.</param>
         public ExecutiveFastLight(Guid execGuid) {
+#if !NETSTANDARD
             NameValueCollection nvc = (NameValueCollection)System.Configuration.ConfigurationManager.GetSection("Sage");
             if (nvc != null)
             {
@@ -162,7 +163,7 @@ namespace Highpoint.Sage.SimCore {
             {
                 Console.WriteLine("No Sage initialization section found in app.config.");
             }
-
+#endif
             m_execGuid = execGuid;
             m_runNumber = 0;
             Reset();
@@ -333,7 +334,12 @@ namespace Highpoint.Sage.SimCore {
 			return RequestEvent(eer,when,priority,userData);
 		}
 
-#region Unrequest Events - not supported
+        public long ResubmitEventAtTime(long eventID, DateTime newTime, bool deleteOldOne)
+        {
+            throw new NotSupportedException("The selected executive type does not support event deletion.");
+        }
+
+        #region Unrequest Events - not supported
         /// <summary>
         /// This executive does not support unrequesting already-submitted event requests.
         /// </summary>

Sage/Core/IExecutive.cs
@@ -312,11 +312,19 @@ namespace Highpoint.Sage.SimCore {
 		/// <param name="execEventType">The way the event is to be served by the executive.</param>
 		/// <returns>A code that can subsequently be used to identify the request, e.g. for removal.</returns>
 		long RequestEvent(ExecEventReceiver eer, DateTime when, double priority, object userData, ExecEventType execEventType);
-		/// <summary>
-		/// Removes an already-submitted request for a time-based notification.
-		/// </summary>
-		/// <param name="eventHashCode">The code that identifies the event request to be removed.</param>
-		void UnRequestEvent(long eventHashCode);
+        /// <summary>
+        /// Resubmits a copy of an event already in queue, optionally .
+        /// </summary>
+        /// <param name="eventID">The ID number assigned to the event when it was initially requested.</param>
+        /// <param name="newTime">The new time.</param>
+        /// <param name="deleteOldOne">If true, will submit the old one for deletion.</param>
+        /// <returns>A code that can subsequently be used to identify the request, e.g. for removal.</returns>
+        long ResubmitEventAtTime(long eventID, DateTime newTime, bool deleteOldOne);
+        /// <summary>
+        /// Removes an already-submitted request for a time-based notification.
+        /// </summary>
+        /// <param name="eventHashCode">The code that identifies the event request to be removed.</param>
+        void UnRequestEvent(long eventHashCode);
 		/// <summary>
 		/// Removes an already-submitted request for a time-based notification based on a user-provided selector object.
 		/// </summary>

Sage/Core/ModelConfig.cs
@@ -32,4 +32,5 @@ namespace Highpoint.Sage.SimCore {
 			return retval;
 		}
 	}
+
 }

Sage/Graphs/Edge.cs
@@ -293,7 +293,7 @@ namespace Highpoint.Sage.Graphs {
 		/// A description of this Edge.
 		/// </summary>
 		public string Description {
-			get { return m_description==null?m_name:m_description; }
+			get { return m_description ?? m_name; }
 		}
 
         /// <summary>
@@ -451,7 +451,7 @@ namespace Highpoint.Sage.Graphs {
         public Edge AddCofinish(Edge slaveEdge) {
 			bool hasVm = (m_vm != null);
 			if ( hasVm ) m_vm.Suspend();
-			Edge retval = AddLigature(PostVertex,slaveEdge.PostVertex);
+			Edge retval = AddLigature(slaveEdge.PostVertex, PostVertex);
 			if ( StructureChangeHandler != null ) StructureChangeHandler(this,StructureChangeType.AddCofinish,false);
 			if ( hasVm ) m_vm.Resume();
 			return retval;
@@ -928,7 +928,7 @@ namespace Highpoint.Sage.Graphs {
         /// <param name="from">The 'from' vertex</param>
         /// <param name="to">The 'to' vertex</param>
         protected static void RemoveLigature(Vertex from, Vertex to) {
-			string name = Ligature.CreateName(from,to);
+			//string name = Ligature.CreateName(from,to);
 			foreach ( Edge e in from.SuccessorEdges ) {
 				if ( e is Ligature && e.Post.Equals(to) ) {
 					((Ligature)e).Disconnect();
@@ -943,7 +943,7 @@ namespace Highpoint.Sage.Graphs {
         /// <param name="from">The 'from' edge.</param>
         /// <param name="to">The 'to' edge.</param>
         protected static void RemoveLigature(Edge from, Edge to) {
-			string name = Ligature.CreateName(from,to);
+			//string name = Ligature.CreateName(from,to);
 			foreach ( Edge e in from.PostVertex.SuccessorEdges ) {
 				if ( e is Ligature && e.PostVertex.Equals(to.PreVertex) ) {
 					((Ligature)e).Disconnect();

commit e1804d689f6bebd4da4cf1a7d3aade0d221fe3c3

TestMementoCaching test
Fix null reference caused by changes in changeset a0546fe

Sage_Aux/SageTestLib/TestSmartPropertyBag.cs
@@ -185,8 +185,7 @@ namespace Highpoint.Sage.SimCore  {
 
 			Debug.WriteLine("\r\nChanging a value in a foreign alias.");
 			spb2["_Pete"]=16;
-            Assert.IsTrue((double)spb1["Marvin"] == 45,"Marvin is not 45");
-
+            
 			Highpoint.Sage.Utility.Mementos.IMemento mem6 = spb1.Memento;
 			Debug.WriteLine("The new snapshot is " + (mem5==mem6?"":"not ") + "equal to the one preceding it.\r\n");
             Assert.IsTrue(mem5!=mem6,"Memento is equal to memento 6");